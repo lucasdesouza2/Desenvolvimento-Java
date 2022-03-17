ce3393494d694762346ac8d6e46f4394ff7b475b
Lucas S <97764224+lucasdesouza2@users.noreply.github.com>
2022-03-16 22:19:18 -0300

    Alteração gitignore
    Criação de classes java

---
+++ EstruturaSequencial/src/Semana1/Ex01Farenheit.java
@@ -0,0 +1,9 @@
+/*Faça um Programa que peça a temperatura em graus Fahrenheit, 
+ * transforme e mostre a temperatura em graus Celsius. C = 5 * ((F-32) / 9)*/
+package Semana1;
+
+public class Ex01Farenheit {
+ public static void main (String []args) {
+	 
+ }
+}
